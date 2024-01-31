c6b122c32315a3114f8f5fb96cfbce2a146aa18b
James Coxhead <james.coxhead@capgemini.com>
2024-01-31 15:09:37 +0000

    feat: update scaffolder header

--- app/packages/app/src/App.tsx
+++ app/packages/app/src/App.tsx
@@ -28,24 +28,30 @@ import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
+import {
+  AlertDisplay,
+  OAuthRequestDialog,
+  SignInPage,
+} from '@backstage/core-components';
 import { createApp } from '@backstage/app-defaults';
 import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
 import { CatalogGraphPage } from '@backstage/plugin-catalog-graph';
 import { RequirePermission } from '@backstage/plugin-permission-react';
-import { catalogEntityCreatePermission, catalogLocationCreatePermission } from '@backstage/plugin-catalog-common/alpha';
+import {
+  catalogEntityCreatePermission,
+  catalogLocationCreatePermission,
+} from '@backstage/plugin-catalog-common/alpha';
 
 import { microsoftAuthApiRef } from '@backstage/core-plugin-api';
-import { SignInPage } from '@backstage/core-components';
 
-import LightIcon from  '@material-ui/icons/WbSunnyRounded';
+import LightIcon from '@material-ui/icons/WbSunnyRounded';
 import NightIcon from '@material-ui/icons/Brightness2Rounded';
 
 import {
   UnifiedThemeProvider,
   createUnifiedTheme,
   palettes,
-  genPageTheme,         
+  genPageTheme,
 } from '@backstage/theme';
 
 import styles from 'style-loader!css-loader?{"modules": {"auto": true}}!sass-loader?{"sassOptions": {"quietDeps": true}}!./style.module.scss';
@@ -54,7 +60,7 @@ import { AdpPage } from '@internal/plugin-adp';
 const lightTheme = createUnifiedTheme({
   palette: {
     ...palettes.light,
-      navigation: {
+    navigation: {
       background: styles.lightThemeNav,
       indicator: styles.primaryColour,
       color: styles.unselectedNavText,
@@ -63,7 +69,7 @@ const lightTheme = createUnifiedTheme({
         hoverBackground: styles.navHoverBackground,
       },
     },
-    primary:{
+    primary: {
       main: styles.primaryColour,
     },
     link: styles.linkColour,
@@ -73,46 +79,46 @@ const lightTheme = createUnifiedTheme({
   defaultPageTheme: 'home',
   pageTheme: {
     home: genPageTheme({ colors: [`${styles.lightThemeNav}`], shape: 'none' }),
-  },  
+  },
   fontFamily: "'GDS Transport',arial, sans-serif",
-  components: { 
+  components: {
     BackstageHeader: {
       styleOverrides: {
         header: {
-          borderBottom: `4px solid ${styles.primaryColour}`, 
-        }
-      }
+          borderBottom: `4px solid ${styles.primaryColour}`,
+        },
+      },
     },
     MuiFormHelperText: {
-      styleOverrides:{
-        root: { 
-           color: styles.secondaryTextColour,
-           "&$error": {
-            color: styles.secondaryTextColour,
-          }
-        }
-      }
-    },
-    MuiInputLabel:{
       styleOverrides: {
-        root: { 
+        root: {
           color: styles.secondaryTextColour,
-        }
-      }
+          '&$error': {
+            color: styles.secondaryTextColour,
+          },
+        },
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: styles.secondaryTextColour,
+        },
+      },
     },
     MuiTypography: {
-      styleOverrides:{
-        caption: { 
+      styleOverrides: {
+        caption: {
           color: ` ${styles.secondaryTextColour} !important`,
-        }
-      }
-    }
+        },
+      },
+    },
   },
 });
 
 const darkTheme = createUnifiedTheme({
   palette: {
-    ...palettes.dark, 
+    ...palettes.dark,
     navigation: {
       background: styles.darkThemeNav,
       indicator: styles.primaryColour,
@@ -128,18 +134,18 @@ const darkTheme = createUnifiedTheme({
   },
   defaultPageTheme: 'home',
   pageTheme: {
-    home: genPageTheme({ colors:  [`${styles.darkThemeNav}`], shape: 'none' }),
+    home: genPageTheme({ colors: [`${styles.darkThemeNav}`], shape: 'none' }),
   },
   fontFamily: "'GDS Transport',arial, sans-serif",
   components: {
     MuiTypography: {
-      styleOverrides:{
-        h2: { 
+      styleOverrides: {
+        h2: {
           color: `${styles.lightGrey} !important`,
         },
-      }
-    }
-  }
+      },
+    },
+  },
 });
 
 const app = createApp({
@@ -152,10 +158,10 @@ const app = createApp({
           id: 'aad-auth-provider',
           title: 'Azure AD',
           message: 'Sign in using Azure AD',
-          apiRef: microsoftAuthApiRef
+          apiRef: microsoftAuthApiRef,
         }}
       />
-    )
+    ),
   },
   apis,
   themes: [
@@ -164,14 +170,18 @@ const app = createApp({
       title: 'Default Light',
       variant: 'light',
       icon: <LightIcon />,
-      Provider: ({ children }) => <UnifiedThemeProvider theme={lightTheme} children={children} />,
+      Provider: ({ children }) => (
+        <UnifiedThemeProvider theme={lightTheme} children={children} />
+      ),
     },
     {
       id: 'default-dark',
       title: 'Default Dark',
       variant: 'dark',
       icon: <NightIcon />,
-      Provider: ({ children }) => <UnifiedThemeProvider theme={darkTheme} children={children} />,
+      Provider: ({ children }) => (
+        <UnifiedThemeProvider theme={darkTheme} children={children} />
+      ),
     },
   ],
   bindRoutes({ bind }) {
@@ -208,18 +218,29 @@ const routes = (
       path="/create"
       element={
         <RequirePermission permission={catalogEntityCreatePermission}>
-          <ScaffolderPage />
+          <ScaffolderPage
+            headerOptions={{
+              title: 'Create a new platform service',
+              subtitle:
+                'Create a new platform service using standard templates in DEFRA',
+            }}
+          />
         </RequirePermission>
       }
     />
     <Route path="/api-docs" element={<ApiExplorerPage />} />
     <Route
       path="/tech-radar"
-      element={<TechRadarPage width={1500} height={800}
-        title='Azure Development Platform: Tech Radar'
-        subtitle="Supported technolgies use approved use of the Defra's Azure Developer Platform"
-        pageTitle='ADP Tech Radar' id='dev'
-        />}
+      element={
+        <TechRadarPage
+          width={1500}
+          height={800}
+          title="Azure Development Platform: Tech Radar"
+          subtitle="Supported technolgies use approved use of the Defra's Azure Developer Platform"
+          pageTitle="ADP Tech Radar"
+          id="dev"
+        />
+      }
     />
     <Route
       path="/catalog-import"
