456f5d3b8b013232c5ee38338b140662c9d219c8
James Coxhead <james.coxhead@capgemini.com>
2024-02-05 16:17:51 +0000

    feat: Update subheading for tech radar

--- app/packages/app/src/App.tsx
+++ app/packages/app/src/App.tsx
@@ -217,7 +217,7 @@ const routes = (
       path="/tech-radar"
       element={<TechRadarPage width={1500} height={800}
         title='Azure Development Platform: Tech Radar'
-        subtitle="Supported technolgies use approved use of the Defra's Azure Developer Platform"
+        subtitle="Supported technologies for DEFRA's Azure Developer Platform"
         pageTitle='ADP Tech Radar' id='dev'
         />}
     />
