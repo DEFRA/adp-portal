4fc25e77bf9c65d4f2577a3fba7f5cc952ddf244
James Coxhead <james.coxhead@capgemini.com>
2024-03-12 11:29:13 +0000

    test: Test coverage for null resources

--- app/plugins/scaffolder-backend-module-adp-scaffolder-actions/src/actions/azure-devops/permitPipeline.test.ts
+++ app/plugins/scaffolder-backend-module-adp-scaffolder-actions/src/actions/azure-devops/permitPipeline.test.ts
@@ -4,6 +4,7 @@ import { permitPipelineAction } from './permitPipeline';
 import { getVoidLogger } from '@backstage/backend-common';
 import { PassThrough } from 'stream';
 import { AzureDevOpsApi } from './AzureDevOpsApi';
+import { createMockActionContext } from '@backstage/plugin-scaffolder-node-test-utils';
 
 describe('adp:azure:pipeline:permit', () => {
   beforeEach(() => {
@@ -100,5 +101,80 @@ describe('adp:azure:pipeline:permit', () => {
     expect(mockContext.logger.info).toHaveBeenLastCalledWith(
       'Updated resource permissions in pipeline 1234',
     );
+  });
+
+  it.each([
+    {
+      variableGroupIds: [111, 222, 333],
+      environmentIds: [444, 555, 666],
+      agentQueueIds: [777, 888, 999],
+    },
+    {
+      serviceConnectionIds: [
+        '7d1286fb-33e7-43ee-b5db-68edd53ac7e1',
+        '83e3836d-eb1e-4a9c-8bd2-b5a473581a2d',
+        'a24305f9-dbd5-4043-8953-1df24e369c62',
+      ],
+      environmentIds: [444, 555, 666],
+      agentQueueIds: [777, 888, 999],
+    },
+    {
+      serviceConnectionIds: [
+        '7d1286fb-33e7-43ee-b5db-68edd53ac7e1',
+        '83e3836d-eb1e-4a9c-8bd2-b5a473581a2d',
+        'a24305f9-dbd5-4043-8953-1df24e369c62',
+      ],
+      variableGroupIds: [111, 222, 333],
+      agentQueueIds: [777, 888, 999],
+    },
+    {
+      serviceConnectionIds: [
+        '7d1286fb-33e7-43ee-b5db-68edd53ac7e1',
+        '83e3836d-eb1e-4a9c-8bd2-b5a473581a2d',
+        'a24305f9-dbd5-4043-8953-1df24e369c62',
+      ],
+      variableGroupIds: [111, 222, 333],
+      environmentIds: [444, 555, 666],
+    },
+  ])('should handle missing resource types', async resources => {
+    const permitSpy = jest
+      .spyOn(AzureDevOpsApi.prototype, 'permitPipeline')
+      .mockResolvedValue([
+        {
+          resource: {
+            id: '1234',
+            name: 'some-resource',
+            type: 'some-resource-type',
+          },
+          allPipelines: {
+            authorized: true,
+            authorizedBy: { name: 'test-user' },
+            authorizedOn: '2024-01-25',
+          },
+          pipelines: [
+            {
+              authorized: true,
+              authorizedBy: { name: 'test-user' },
+              authorizedOn: '2024-01-25',
+              id: 1111,
+            },
+          ],
+        },
+      ]);
+
+    const context = {
+      ...mockContext,
+      input: {
+        ...mockContext.input,
+        resources: resources,
+      },
+    };
+
+    const loggerSpy = jest.spyOn(mockContext.logger, 'info');
+
+    await action.handler(context);
+
+    expect(permitSpy).toHaveBeenCalled();
+    expect(loggerSpy).toHaveBeenCalled();
   });
 });
