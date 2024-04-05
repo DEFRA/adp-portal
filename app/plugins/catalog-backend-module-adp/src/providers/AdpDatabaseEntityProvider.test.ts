41a0c01789bd4e919fc41b8b7c90f7838b13953b
Rozerin <rozerin.baran@capgemini.com>
2024-04-05 14:12:18 +0100

    added children string array for programme & entity provider uses api for project and programmes instead of db

--- app/plugins/catalog-backend-module-adp/src/providers/AdpDatabaseEntityProvider.test.ts
+++ app/plugins/catalog-backend-module-adp/src/providers/AdpDatabaseEntityProvider.test.ts
@@ -102,6 +102,7 @@ describe('AdbDatabaseEntityProvider', () => {
           description: 'Test description 1',
           url: 'https://test1.com',
           title: 'Test ALB 1',
+          children: ['test-title-1']
         },
         {
           id: '2222',
@@ -113,7 +114,58 @@ describe('AdbDatabaseEntityProvider', () => {
           description: 'Test description 2',
           url: 'https://test2.com',
           title: 'Test ALB 2',
+          children: ['test-title-2']
         },
+        {
+          programme_managers: [],
+          title: 'Test title 1',
+          alias: 'Test Alias',
+          description: 'Test description',
+          finance_code: 'Test finance_code',
+          arms_length_body_id: '1111',
+          delivery_programme_code: 'Test delivery_programme_code',
+          url: 'https://www.example.uk/',
+          name: 'test-title-1',
+          id: '123',
+          children: ['test-title-1']
+        },
+        {
+          programme_managers: [],
+          title: 'Test title 1',
+          alias: 'Test Alias',
+          description: 'Test description',
+          finance_code: 'Test finance_code',
+          arms_length_body_id: '2222',
+          delivery_programme_code: 'Test delivery_programme_code',
+          url: 'https://www.example.uk/',
+          name: 'test-title-1',
+          id: '1234',
+          children: ['test-title-2']
+        },
+        {
+          title: 'Test title 1',
+          alias: 'Test Alias',
+          description: 'Test description',
+          finance_code: 'Test finance_code',
+          delivery_programme_id: '123`',
+          delivery_project_code: 'Test delivery_project_code',
+          url: 'https://www.example.uk/',
+          name: 'test-title-1',
+          id: '123',
+          children: []
+        },
+        {
+          title: 'Test title 2',
+          alias: 'Test Alias',
+          description: 'Test description',
+          finance_code: 'Test finance_code',
+          delivery_programme_id: '1234',
+          delivery_project_code: 'Test delivery_project_code',
+          url: 'https://www.example.uk/',
+          name: 'test-title-1',
+          id: '1234',
+          children: []
+        }
       ]),
       ok: jest.fn().mockImplementation(() => true),
     } as unknown as Response);
@@ -150,7 +202,7 @@ describe('AdbDatabaseEntityProvider', () => {
           },
           spec: {
             type: 'arms-length-body',
-            children: [],
+            children: ['test-title-1'],
           },
         },
         locationKey: entityProvider.getProviderName(),
@@ -174,6 +226,100 @@ describe('AdbDatabaseEntityProvider', () => {
           },
           spec: {
             type: 'arms-length-body',
+            children: ['test-title-2'],
+          },
+        },
+        locationKey: entityProvider.getProviderName(),
+      },
+      {
+        entity: {
+          apiVersion: 'backstage.io/v1beta1',
+          kind: 'Group',
+          metadata: {
+            name: 'test-title-1',
+            title: 'Test title 1 (Test Alias)',
+            description: 'Test description',
+            tags: [],
+            annotations: {
+              'backstage.io/managed-by-location':
+                'adp:delivery-programme\\test-title-1',
+              'backstage.io/managed-by-origin-location':
+                `adp:delivery-programme\\test-title-1`,
+            },
+            links: [{ url: 'https://www.example.uk/' }],
+          },
+          spec: {
+            type: 'delivery-programme',
+            children: ['test-title-1'],
+          },
+        },
+        locationKey: entityProvider.getProviderName(),
+      },
+      {
+        entity: {
+          apiVersion: 'backstage.io/v1beta1',
+          kind: 'Group',
+          metadata: {
+            name: 'test-title-2',
+            title: 'Test title 2 (Test Alias)',
+            description: 'Test description',
+            tags: [],
+            annotations: {
+              'backstage.io/managed-by-location':
+                'adp:delivery-programme\\test-title-2',
+              'backstage.io/managed-by-origin-location':
+                `adp:delivery-programme\\test-title-2`,
+            },
+            links: [{ url: 'https://www.example.uk/' }],
+          },
+          spec: {
+            type: 'delivery-programme',
+            children: ['test-title-2'],
+          },
+        },
+        locationKey: entityProvider.getProviderName(),
+      },
+      {
+        entity: {
+          apiVersion: 'backstage.io/v1beta1',
+          kind: 'Group',
+          metadata: {
+            name: 'test-title-1',
+            title: 'Test title 1 (Test Alias)',
+            description: 'Test description',
+            tags: [],
+            annotations: {
+              'backstage.io/managed-by-location':
+                'adp:delivery-project\\test-title-1',
+              'backstage.io/managed-by-origin-location': `adp:delivery-project\\test-title-1`,
+            },
+            links: [{ url: 'https://www.example.uk/' }],
+          },
+          spec: {
+            type: 'delivery-project',
+            children: [],
+          },
+        },
+        locationKey: entityProvider.getProviderName(),
+      },
+      {
+        entity: {
+          apiVersion: 'backstage.io/v1beta1',
+          kind: 'Group',
+          metadata: {
+            name: 'test-title-2',
+            title: 'Test title 2 (Test Alias)',
+            description: 'Test description',
+            tags: [],
+            annotations: {
+              'backstage.io/managed-by-location':
+                'adp:delivery-project\\test-title-2',
+              'backstage.io/managed-by-origin-location': `adp:delivery-project\\test-title-2`,
+            },
+            links: [{ url: 'https://www.example.uk/' }],
+          },
+          spec: {
+            type: 'delivery-project',
             children: [],
           },
         },
