135b5bee5139033e036b36f686e3c37cf903c526
Rozerin <rozerin.baran@capgemini.com>
2024-04-16 14:03:08 +0100

    covered empty children array scenario in tests

--- app/plugins/catalog-backend-module-adp/src/transformers/deliveryProjectTransformers.test.ts
+++ app/plugins/catalog-backend-module-adp/src/transformers/deliveryProjectTransformers.test.ts
@@ -1,46 +1,12 @@
+import {
+  deliveryProject,
+  expectedProjectEntity,
+} from '../testData/projectTransformerTestData';
 import { deliveryProjectGroupTransformer } from './deliveryProjectTransformer';
 
 describe('deliveryProjectGroupTransformer', () => {
   it('should transform a valid DeliveryProject to a GroupEntity', async () => {
-    const deliveryProject = {
-      delivery_programme_id: '05aa36b6-c7a2-4c35-820e-c31d20011f47',
-      name: 'test-title-1',
-      id: '1234',
-      title: 'Test title 1',
-      alias: 'Test Alias',
-      description: 'Test description',
-      finance_code: 'Test finance_code',
-      delivery_project_code: 'Test delivery_project_code',
-      namespace: 'Test namespace',
-      ado_project: 'Test ado_project',
-      team_type: 'Test team_type',
-      service_owner: 'Test service_owner',
-      created_at: new Date(),
-      updated_at: new Date(),
-    };
-
-    const expectedGroupEntity = {
-      apiVersion: 'backstage.io/v1beta1',
-      kind: 'Group',
-      metadata: {
-        name: 'test-title-1',
-        title: 'Test title 1 (Test Alias)',
-        description: 'Test description',
-        tags: [],
-        annotations: {
-          'backstage.io/managed-by-location':
-            'adp:delivery-project\\test-title-1',
-          'backstage.io/managed-by-origin-location': `adp:delivery-project\\test-title-1`,
-        },
-        links: [],
-      },
-      spec: {
-        type: 'delivery-project',
-        children: [],
-      },
-    };
-
     const result = await deliveryProjectGroupTransformer(deliveryProject);
-    expect(result).toEqual(expectedGroupEntity);
+    expect(result).toEqual(expectedProjectEntity);
   });
 });
