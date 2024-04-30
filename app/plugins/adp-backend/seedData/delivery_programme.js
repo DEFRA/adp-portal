612d41f4ab765536608dc070f12e88cc35f22c6b
Rozerin <rozerin.baran@capgemini.com>
2024-04-30 15:38:54 +0100

    seed file for programmes will check empty fields and populate required fields, urlSlug, removed  non-alphaneumeric values restriction

--- app/plugins/adp-backend/seedData/delivery_programme.js
+++ app/plugins/adp-backend/seedData/delivery_programme.js
@@ -4,6 +4,14 @@ const TABLE_NAME = 'delivery_programme';
  * @param {import('knex').Knex} knex
  */
 exports.seed = async function seed(knex) {
+
+  const recordsWithEmptyFields = await knex(TABLE_NAME)
+  .select('name')
+  .where(function() {
+    this.where('description', '')
+    .orWhere('delivery_programme_code', '')
+});
+
   for (const record of records) {
     const [{ count }] = await knex(TABLE_NAME)
       .where('name', record.name)
@@ -11,6 +19,15 @@ exports.seed = async function seed(knex) {
       .count('*', { as: 'count' });
     if (parseInt(count) === 0) {
       await knex(TABLE_NAME).insert(await resolveReferences(record));
+    }
+    const recordsToUpdate = recordsWithEmptyFields.filter(d => d.name === record.name)
+    if(recordsToUpdate.length > 0) {
+      await knex(TABLE_NAME)
+      .whereIn('name', recordsToUpdate)
+      .update({
+        description: record.description,
+        delivery_programme_code: record.delivery_programme_code,
+      })      
     }
   }
 
@@ -25,7 +42,7 @@ exports.seed = async function seed(knex) {
       arms_length_body_id: albId[0].id,
     };
   }
-};
+}
 
 const records = [
   {
@@ -54,7 +71,7 @@ const records = [
     name: 'extended-producer-responsibility',
     title: 'Extended Producer Responsibility',
     alias: 'EPR',
-    description: '',
+    description: 'EPR will move the full cost of dealing with packaging waste from households away from local taxpayers and councils to the packaging producers, giving producers responsibility for the costs of their packaging throughout its life cycle.',
     finance_code: null,
     arms_length_body_id: 'environment-agency',
     delivery_programme_code: '',
@@ -76,10 +93,10 @@ const records = [
     name: 'biodiversity-net-gains',
     title: 'Biodiversity & Net Gains',
     alias: 'BNG',
-    description: '',
+    description: 'Biodiversity net gain (BNG) is a way of creating and improving natural habitats. BNG makes sure development has a measurably positive impact (‘net gain’) on biodiversity, compared to what was there before development.',
     finance_code: null,
     arms_length_body_id: 'natural-england',
-    delivery_programme_code: '',
+    delivery_programme_code: 'BNG',
     url: null,
     updated_by: 'ADP',
   },
@@ -87,7 +104,7 @@ const records = [
     name: 'farming-countryside-programme',
     title: 'Farming & Countryside Programme',
     alias: 'FCP',
-    description: '',
+    description: 'The Farming and Countryside Programme is responsible for designing and delivering the new farming schemes in England.',
     finance_code: null,
     arms_length_body_id: 'rural-payments-agency',
     delivery_programme_code: '',
