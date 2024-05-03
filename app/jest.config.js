bc24c1dd43e529edcf60b83d90a3e04e3541a8d1
Danny May <danny.may@capgemini.com>
2024-05-03 11:54:29 +0000

    Consolidate jest config

--- app/jest.config.js
+++ app/jest.config.js
@@ -1,10 +1,27 @@
 process.env.TZ = 'UTC';
 
-/** @type {Promise<unknown>} */
+/** @type {Promise<import('jest').Config>} */
 const baseConfig = require('@backstage/cli/config/jest.js');
 
 module.exports = (async function () {
+  /** @type {import('jest').Config} */
   return {
     ...(await baseConfig),
+    testPathIgnorePatterns: [
+      '/packages/backend/src/plugins/',
+      '/packages/backend/src/index.ts',
+      '/packages/app/src/apis.ts',
+      'module.ts',
+      'run.ts',
+      'standaloneServer.ts',
+    ],
+    coveragePathIgnorePatterns: [
+      '/packages/backend/src/plugins/',
+      '/packages/backend/src/index.ts',
+      '/packages/app/src/apis.ts',
+      'module.ts',
+      'run.ts',
+      'standaloneServer.ts',
+    ],
   };
 })();
