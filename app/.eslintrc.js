cd97f7032813a2845fc343c92460eea5ed48ffd1
Danny May <danny.may@capgemini.com>
2024-05-03 23:53:30 +0000

    Cleanup

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,8 +1,11 @@
 /** @type {import('eslint').Linter.Config} */
-module.exports = require('@backstage/cli/config/eslint-factory').createConfig(__dirname, {
-  root: true,
-  ignorePatterns: ['**/seedData/', '**/migrations/'],
-  rules: {
-    '@typescript-eslint/consistent-type-imports': 'error',
+module.exports = require('@backstage/cli/config/eslint-factory').createConfig(
+  __dirname,
+  {
+    root: true,
+    ignorePatterns: ['**/seedData/', '**/migrations/'],
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+    },
   },
-});
+);
