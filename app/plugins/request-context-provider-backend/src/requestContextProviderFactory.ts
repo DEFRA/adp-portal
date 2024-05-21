f7ae56c70e9f46907e26e87d41afd4a77f7d8f52
danny-may <danny.may@capgemini.com>
2024-05-21 23:36:29 +0100

    Update docs

--- app/plugins/request-context-provider-backend/src/requestContextProviderFactory.ts
+++ app/plugins/request-context-provider-backend/src/requestContextProviderFactory.ts
@@ -20,10 +20,10 @@ export const requestContextProviderFactory = createServiceFactory({
 });
 
 /**
- * Backstage doesnt allow multiple middleware to be installed onto the root handler,
- * so this method tricks it into thinking the first arg is a unique string, which is
- * passed onto express as the first parameter. Express will detect this first arg as
- * a function, and so not be tricked like backstage was.
+ * Backstage doesnt allow multiple handlers to be installed onto the same path on the
+ * root handler, so this method tricks it into thinking the first arg is a unique
+ * string, which is passed onto express as the first parameter. Express will detect
+ * this first arg as a function, and so not be tricked like backstage was.
  *
  * This isnt needed if this service is the only thing installing into the root. Its
  * only being done to prevent conflicts with other plugins/services.
