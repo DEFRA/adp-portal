220b6f32d907e72b7795fa189fea6cc7c1fdee18
rajeshkaremane <rajesh.venkatraman@capgemini.com>
2024-05-24 16:39:07 +0100

    export module

--- app/packages/backend/src/opentelemetry.js
+++ app/packages/backend/src/opentelemetry.js
@@ -1,36 +1,45 @@
-const { metrics, trace } = require('@opentelemetry/api')
-const { registerInstrumentations } = require('@opentelemetry/instrumentation')
-const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express')
-const { useAzureMonitor } = require('@azure/monitor-opentelemetry')
-const { SEMRESATTRS_SERVICE_NAME } = require('@opentelemetry/semantic-conventions')
-const { Resource } = require('@opentelemetry/resources')
+const { metrics, trace } = require('@opentelemetry/api');
+const { registerInstrumentations } = require('@opentelemetry/instrumentation');
+const {
+  ExpressInstrumentation,
+} = require('@opentelemetry/instrumentation-express');
+const { useAzureMonitor } = require('@azure/monitor-opentelemetry');
+const {
+  SEMRESATTRS_SERVICE_NAME,
+} = require('@opentelemetry/semantic-conventions');
+const { Resource } = require('@opentelemetry/resources');
 const resource = new Resource({
-  [SEMRESATTRS_SERVICE_NAME]: process.env.APPINSIGHTS_CLOUDROLE || 'defra-adp-portal'
-})
-const { ManagedIdentityCredential } = require('@azure/identity')
+  [SEMRESATTRS_SERVICE_NAME]:
+    process.env.APPINSIGHTS_CLOUDROLE || 'defra-adp-portal',
+});
+const { ManagedIdentityCredential } = require('@azure/identity');
 
-export function setupOpenTelemetry() {
+function setupOpenTelemetry() {
   if (process.env.SHARED_APPINSIGHT_CONNECTIONSTRING) {
     const options = {
       azureMonitorExporterOptions: {
-        connectionString:
-          process.env.SHARED_APPINSIGHT_CONNECTIONSTRING,
-        credential: (process.env.NODE_ENV === 'production') ? new ManagedIdentityCredential(process.env.ADP_PORTAL_MI_CLIENT_ID) : undefined
+        connectionString: process.env.SHARED_APPINSIGHT_CONNECTIONSTRING,
+        credential:
+          process.env.NODE_ENV === 'production'
+            ? new ManagedIdentityCredential(process.env.ADP_PORTAL_MI_CLIENT_ID)
+            : undefined,
       },
-      resource
-    }
-    useAzureMonitor(options)
-    const instrumentations = [
-      new ExpressInstrumentation()
-    ]
+      resource: resource,
+    };
+    useAzureMonitor(options);
+    const instrumentations = [new ExpressInstrumentation()];
     registerInstrumentations({
       tracerProvider: trace.getTracerProvider(),
       meterProvider: metrics.getMeterProvider(),
-      instrumentations
-    })
-    console.log('App Insights Running')
+      instrumentations: instrumentations,
+    });
+    console.log('App Insights Running');
   } else {
-    console.log('App Insights Not Running!')
+    console.log('App Insights Not Running!');
   }
 }
-setupOpenTelemetry()
+setupOpenTelemetry();
+
+module.exports = {
+  setupOpenTelemetry,
+};
