38061668ca16cd639e8be7ce7a66ed9d35c77754
James Coxhead <james.coxhead@capgemini.com>
2024-06-05 16:50:20 +0100

    refactor: Permissions customisations into own module

--- app/packages/backend/src/index.ts
+++ app/packages/backend/src/index.ts
@@ -13,7 +13,6 @@ import fetchApiFactory, {
 } from '@internal/plugin-fetch-api-backend';
 import {
   addAdoNameTransformer,
-  addAdpPermissionsPolicy,
   addCatalogPermissionRules,
   addScaffolderModuleAdpActions,
 } from './modules';
@@ -64,7 +63,7 @@ backend.add(
 backend.add(import('@backstage/plugin-auth-backend'));
 backend.add(import('@backstage/plugin-auth-backend-module-microsoft-provider'));
 backend.add(import('@backstage/plugin-permission-backend/alpha'));
-backend.add(addAdpPermissionsPolicy);
+backend.add(import('@internal/plugin-permission-backend-module-adp'));
 backend.add(addCatalogPermissionRules);
 
 // Backstage
--- app/packages/backend/src/permissions/index.ts
+++ app/packages/backend/src/permissions/index.ts
@@ -1,4 +1,1 @@
-export * from './policies';
 export * from './rules';
-export { RbacUtilities } from './rbacUtilites';
-export type { RbacGroups } from './types';
