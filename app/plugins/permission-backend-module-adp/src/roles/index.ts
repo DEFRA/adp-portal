38aefb01236d195ff897238fea108139a35de6a1
James Coxhead <james.coxhead@capgemini.com>
2024-06-20 11:59:49 +0100

    refactor: permission policy roles

---
+++ app/plugins/permission-backend-module-adp/src/roles/index.ts
@@ -0,0 +1,6 @@
+export * from './catalogReaderRole';
+export * from './deliveryProgrammeAdminMangerRole';
+export * from './deliveryProgrammeAdminRole';
+export * from './deliveryProjectUserManagerRole';
+export * from './platformAdminRole';
+export * from './scaffolderUsersRole';
