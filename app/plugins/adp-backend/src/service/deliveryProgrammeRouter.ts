f5534bc08b4afb95e318b415f23a366eedcc5c66
Rozerin <rozerin.baran@capgemini.com>
2024-06-28 12:58:11 +0100

    removed unreqiored errorMapping

--- app/plugins/adp-backend/src/service/deliveryProgrammeRouter.ts
+++ app/plugins/adp-backend/src/service/deliveryProgrammeRouter.ts
@@ -68,18 +68,6 @@ const errorMapping = {
       message: `The arms length body does not exist.`,
     },
   }),
-  unknownDeliveryProgramme: () => ({
-    path: 'delivery_programme_id',
-    error: {
-      message: `The delivery programme does not exist.`,
-    },
-  }),
-  unknownCatalogUser: (req: { user_catalog_name?: string }) => ({
-    path: 'user_catalog_name',
-    error: {
-      message: `The user ${req.user_catalog_name} could not be found in the Catalog`,
-    },
-  }),
   unknown: () => ({
     path: 'root',
     error: {
